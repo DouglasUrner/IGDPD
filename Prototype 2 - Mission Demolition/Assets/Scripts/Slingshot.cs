5c79ff85c21d9d9ffedd975287d7a7d9d8d76c8c
Douglas Urner <dlu@canishe.com>
2017-12-28 17:26:07 -0800

    P2: Aim and launch projectile (pgs 508-511).

--- Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
+++ Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -6,6 +6,7 @@ public class Slingshot : MonoBehaviour {
 	// fields set in the Unity Inspector pane
 	[Header("Set in Inspector")]
 	public GameObject prefabProjectile;
+	public float velocityMult = 8f;
 	
 	// fields set dynamically
 	[Header("Set dynamically")]
@@ -13,6 +14,8 @@ public class Slingshot : MonoBehaviour {
 	public Vector3 launchPos;
 	public GameObject projectile;
 	public bool aimingMode;
+
+	private Rigidbody projectileRigidbody;
 	
 	void Awake() {
 		Transform launchPointTrans = transform.Find("LaunchPoint");
@@ -39,6 +42,38 @@ public class Slingshot : MonoBehaviour {
 		// Start it at the launchPoint
 		projectile.transform.position = launchPos;
 		// Set it it isKinematic for now
-		projectile.GetComponent<Rigidbody>().isKinematic = true;
+		projectileRigidbody = projectile.GetComponent<Rigidbody>();
+		projectileRigidbody.isKinematic = true;
+	}
+
+	void Update() {
+		// If Slingshot is not in aimingMode don't run this code
+		if (!aimingMode) return;
+
+		// Get the current mouse position in 2D screen coordinates
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+		
+		// Find the delta from the launchPos to the MousePos3D
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		// Limit mouseDelta to the radius of the Slingshot SphereCollider
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize();
+			mouseDelta *= maxMagnitude;
+		}
+		
+		// Move the projectile to this new position
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		if (Input.GetMouseButtonUp(0)) {
+			// The mouse has been released
+			aimingMode = false;
+			projectileRigidbody.isKinematic = false;
+			projectileRigidbody.velocity = -mouseDelta * velocityMult;
+			projectile = null;
+		}
 	}
 }
