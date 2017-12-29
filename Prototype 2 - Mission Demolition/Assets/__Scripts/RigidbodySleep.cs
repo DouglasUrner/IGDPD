0c68d1b418c558480b2d69f233830ef8dcd93e3a
Douglas Urner <dlu@canishe.com>
2017-12-28 23:40:29 -0800

    P2: Building the Castle (pgs 524-526)
    Set up to build castle (pgs 524-525).

---
+++ Prototype 2 - Mission Demolition/Assets/__Scripts/RigidbodySleep.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour {
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		if (rb != null) rb.Sleep();
+	}
+}
