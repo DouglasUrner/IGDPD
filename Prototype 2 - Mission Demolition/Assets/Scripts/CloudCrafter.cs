81db9c7a187477c77d0e0c69e30aa3f20ec20c5f
Douglas Urner <dlu@canishe.com>
2017-12-28 22:39:58 -0800

    P2: Providing Vection and a Sense of Speed (pgs 516-523).
    Start building weather systems - Crafting Many Clouds (pgs 521-523).

---
+++ Prototype 2 - Mission Demolition/Assets/Scripts/CloudCrafter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour {
+	[Header("Set in Inspector")]
+	public int numClouds = 40;          // The # of clouds to make
+	public GameObject cloudPrefab;      // The prefab for the clouds
+	public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+	public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+	public float cloudScaleMin = 1;     // Min scale on each cloud
+	public float cloudScaleMax = 3;     // Max scale on each cloud
+	public float cloudSpeedMult = 0.5f; // Adjusts the speed of clouds
+
+	private GameObject[] cloudInstances;
+
+	void Awake() {
+		
+	}
+	
+	void Update() {
+		
+	}
+}
