3bc0d9492f9baf1f47bda34961aff3f3199a4cc1
Douglas Urner <dlu@canishe.com>
2017-12-28 16:19:38 -0800

    P2: Initial Slingshot class (pg 503).

---
+++ Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+	void OnMouseEnter() {
+		print("Slingshot:OnMouseEnter()");
+	}
+	
+	void OnMouseExit() {
+		print("Slingshot:OnMouseExit()");
+	}
+}
