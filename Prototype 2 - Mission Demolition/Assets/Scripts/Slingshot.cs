f00a35989318e43cf57fdfe866e2df6a47558497
Douglas Urner <dlu@canishe.com>
2017-12-28 16:35:10 -0800

    P2: Show interactible range of Slingshot with a Halo (pgs 503-505).
    Turn Halo on and off as mouse enters and leaves active range.

--- Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
+++ Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+	public GameObject launchPoint;
+
+	void Awake() {
+		Transform launchPointTrans = transform.Find("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+	}
 
 	void OnMouseEnter() {
-		print("Slingshot:OnMouseEnter()");
+		// print("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive(true);
 	}
 	
 	void OnMouseExit() {
-		print("Slingshot:OnMouseExit()");
+		// print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive(false);
 	}
 }
