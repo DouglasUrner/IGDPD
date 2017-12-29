e88cf3f466818ab9cdf73b54716676055732374c
Douglas Urner <dlu@canishe.com>
2017-12-28 16:55:15 -0800

    P2: Instatitate a projectile (pgs 505-507).

--- Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
+++ Prototype 2 - Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+	// fields set in the Unity Inspector pane
+	[Header("Set in Inspector")]
+	public GameObject prefabProjectile;
+	
+	// fields set dynamically
+	[Header("Set dynamically")]
 	public GameObject launchPoint;
-
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+	
 	void Awake() {
 		Transform launchPointTrans = transform.Find("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive(false);
+		launchPos = launchPointTrans.position;
 	}
 
 	void OnMouseEnter() {
@@ -19,5 +29,16 @@ public class Slingshot : MonoBehaviour {
 	void OnMouseExit() {
 		// print("Slingshot:OnMouseExit()");
 		launchPoint.SetActive(false);
+	}
+
+	void OnMouseDown() {
+		// The player has pressed the mouse button while over the Slingshot
+		aimingMode = true;
+		// Instantiate a projectile
+		projectile = Instantiate(prefabProjectile) as GameObject;
+		// Start it at the launchPoint
+		projectile.transform.position = launchPos;
+		// Set it it isKinematic for now
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
 	}
 }
