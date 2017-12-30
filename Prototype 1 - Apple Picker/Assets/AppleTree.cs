c527047dc5f5ba220e2cd25609bc6a2c41bc0838
Douglas Urner <dlu@canishe.com>
2017-12-29 17:15:11 -0800

    P1: Coding the Apple Picker Prototype (pgs 470-484) - Initial setup.
    Start AppleTree script and lint to the GameObject (pgs 470-472).

--- Prototype 1 - Apple Picker/Assets/AppleTree.cs
+++ Prototype 1 - Apple Picker/Assets/AppleTree.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
+	[Header("Set in Inspector")]
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+	
+	// Speed at which the AppleTree moves
+	public float speed = 1f;
+	
+	// Distance where the AppleTree turns around
+	public float leftAndRightEdge = 10f;
+	
+	// Chance that the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+	
+	// Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
 
-	// Use this for initialization
 	void Start () {
-		
+		// Dropping apples every second
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		// Basic Movement
+		// Changing Direction
 	}
 }
