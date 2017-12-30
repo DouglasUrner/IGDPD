3f0f23c57b12e4f1bd7c998ccfe3e5adeb076fa1
Douglas Urner <dlu@canishe.com>
2017-12-29 23:55:05 -0800

    P1: Coding the Apple Picker Prototype (pgs 470-484) - Move Baskets
    Moving the Baskets with the Mouse (pgs 482-483).

--- Prototype 1 - Apple Picker/Assets/Basket.cs
+++ Prototype 1 - Apple Picker/Assets/Basket.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Basket : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void Update () {
+		// Get the current screen position of the mouse from input
+		Vector3 mousePos2D = Input.mousePosition;
 		
+		// The Camera's z position sets how far to push the mouse into 3D
+		mousePos2D.z = Camera.main.transform.position.z;
+		
+		// Convert the point from 2D screen space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+		
+		// Move the x position of this Basket to the x position of the Mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
 	}
 }
