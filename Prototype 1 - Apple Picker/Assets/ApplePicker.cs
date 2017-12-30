83377d7d2186051e1fe2a2f4c3b8af42ee9b2430
Douglas Urner <dlu@canishe.com>
2017-12-29 23:39:52 -0800

    P1: Coding the Apple Picker Prototype (pgs 470-484) - Instantiate Baskets
    Instantiating the Baskets (pgs 481-482).

--- Prototype 1 - Apple Picker/Assets/ApplePicker.cs
+++ Prototype 1 - Apple Picker/Assets/ApplePicker.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ApplePicker : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	[Header("Set in Inspector")]
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start () {
+		for (int i = 0; i < numBaskets; i++) {
+			GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGO.transform.position = pos;
+		}
 	}
 }
