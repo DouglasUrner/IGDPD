16e4905db8d166485b19da3ae75e7dc8b73e9757
Douglas Urner <dlu@canishe.com>
2017-12-29 20:30:53 -0800

    P1: Coding the Apple Picker Prototype (pgs 470-484) - Changing Direction
    Changing Direction (pgs 474-475).

--- Prototype 1 - Apple Picker/Assets/AppleTree.cs
+++ Prototype 1 - Apple Picker/Assets/AppleTree.cs
@@ -3,32 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
-	[Header("Set in Inspector")]
-	// Prefab for instantiating apples
-	public GameObject applePrefab;
-	
-	// Speed at which the AppleTree moves
-	public float speed = 1f;
-	
-	// Distance where the AppleTree turns around
-	public float leftAndRightEdge = 10f;
-	
-	// Chance that the AppleTree will change directions
-	public float chanceToChangeDirections = 0.1f;
-	
-	// Rate at which Apples will be instantiated
-	public float secondsBetweenAppleDrops = 1f;
+  [Header("Set in Inspector")]
+  // Prefab for instantiating apples
+  public GameObject applePrefab;
 
-	void Start () {
-		// Dropping apples every second
-	}
-	
-	void Update () {
-		// Basic Movement
-		Vector3 pos = transform.position;
-		pos.x += speed * Time.deltaTime;
-		transform.position = pos;
-		
-		// Changing Direction
-	}
+  // Speed at which the AppleTree moves
+  public float speed = 1f;
+
+  // Distance where the AppleTree turns around
+  public float leftAndRightEdge = 10f;
+
+  // Chance that the AppleTree will change directions
+  public float chanceToChangeDirections = 0.1f;
+
+  // Rate at which Apples will be instantiated
+  public float secondsBetweenAppleDrops = 1f;
+
+  void Start() {
+    // Dropping apples every second
+  }
+
+  void Update() {
+    // Basic Movement
+    Vector3 pos = transform.position;
+    pos.x += speed * Time.deltaTime;
+    transform.position = pos;
+
+    // Changing Direction
+    if (pos.x < -leftAndRightEdge) {
+      speed = Mathf.Abs(speed); // Move right
+    } else if (pos.x > leftAndRightEdge) {
+      speed = -Mathf.Abs(speed); // Move left
+    }
+  }
 }
