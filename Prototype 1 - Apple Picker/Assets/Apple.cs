6da8b64641c73caaceb57a1caf612c9137649936
Douglas Urner <dlu@canishe.com>
2017-12-29 23:15:32 -0800

    P1: Coding the Apple Picker Prototype (pgs 470-484) - Stopping Apples
    Stopping Apples If They Fall Too Far (pgs 480-481).

--- Prototype 1 - Apple Picker/Assets/Apple.cs
+++ Prototype 1 - Apple Picker/Assets/Apple.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour {
+	public static float bottomY = -20f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		
+		if (transform.position.y < bottomY) {
+			Destroy(this.gameObject);
+		}
 	}
 }
