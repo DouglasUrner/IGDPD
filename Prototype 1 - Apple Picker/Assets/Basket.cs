4905e740e69d81a6bb91f211d01fafc0b0d4f89c
Douglas Urner <dlu@canishe.com>
2017-12-31 18:55:27 -0800

    P1: GUI and Game Management (pgs 484-494) - Points for Catching
    Add Points for Each Caught Apple (pgs 486-488).

--- Prototype 1 - Apple Picker/Assets/Basket.cs
+++ Prototype 1 - Apple Picker/Assets/Basket.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
-	
+	[Header("Set Dynamically")]
+	public Text scoreGT;
+
+	private void Start() {
+		// Find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find("ScoreCounter");
+		// Get the Text Component of the GameObject
+		scoreGT = scoreGO.GetComponent<Text>();
+		// Set the starting number of points to 0
+		scoreGT.text = "0";
+	}
+
 	void Update () {
 		// Get the current screen position of the mouse from input
 		Vector3 mousePos2D = Input.mousePosition;
@@ -25,6 +37,13 @@ public class Basket : MonoBehaviour {
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") {
 			Destroy(collidedWith);
+			
+			// Parse the text of the scoreGT into an int
+			int score = int.Parse(scoreGT.text);
+			// Add points for catching the Apple
+			score += 100;
+			// Convert the score back into a string and display it
+			scoreGT.text = score.ToString();
 		}
 	}
 }
