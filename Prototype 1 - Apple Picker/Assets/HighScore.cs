b8143912c46b99450913ec4144f8e2da5af4b844
Douglas Urner <dlu@canishe.com>
2017-12-31 20:51:25 -0800

    P1: GUI and Game Management (pgs 484-494) - Save High Score
    Preserving the High Score in PlayerPrefs (pgs 492-494).

--- Prototype 1 - Apple Picker/Assets/HighScore.cs
+++ Prototype 1 - Apple Picker/Assets/HighScore.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour {
 	static public int score = 1000;
-	
+
+	private void Awake() {
+		// If the PlayerPrefs HighScore already exists, read it
+		if (PlayerPrefs.HasKey("HighScore")) {
+			score = PlayerPrefs.GetInt("HighScore");
+		}
+		// Assign the high score to HighScore
+		PlayerPrefs.SetInt("HighScore", score);
+	}
+
 	void Update () {
 		Text gt = this.GetComponent<Text>();
 		gt.text = "High Score: " + score;
+		// Update the PlayerPrefs HighScore if necessary
+		if (score > PlayerPrefs.GetInt("HighScore")) {
+			PlayerPrefs.SetInt("HighScore", score);
+		}
 	}
 }
