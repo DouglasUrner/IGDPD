0085ac93a6dab613e9de23dcd5cf477abf25f543
Douglas Urner <urner@skschools.org>
2018-01-03 10:34:28 -0800

    P6: Building a Level with the WordLevel Class  (pgs 765-769).

--- Prototype 6 - Word Game/Assets/__Scripts/WordGame.cs
+++ Prototype 6 - Word Game/Assets/__Scripts/WordGame.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public enum GameMode {
@@ -34,6 +35,59 @@ public class WordGame : MonoBehaviour {
 	}
 
 	public WordLevel MakeWordLevel(int levelNum = -1) {
+		WordLevel level = new WordLevel();
+		if (levelNum == -1) {
+			// Pick random level
+			level.longWordIndex = Random.Range(0, WordList.LONG_WORD_COUNT);
+		}
+		else {
+			// This will be added later in the chapter
+		}
+
+		level.levelNum = levelNum;
+		level.word = WordList.GET_LONG_WORD(level.longWordIndex);
+		level.charDict = WordLevel.MakeCharDict(level.word);
+
+		StartCoroutine(FindSubWordsCoroutine(level));
+
+		return level;
+	}
+	
+	// A coroutine that finds words that can be spelled in this level
+	public IEnumerator FindSubWordsCoroutine(WordLevel level) {
+		level.subWords = new List<string>();
+		string str;
+
+		List<string> words = WordList.GET_WORDS();
 		
+		// Iterate through all of the words in the wordList
+		for (int i = 0; i < WordList.WORD_COUNT; i++) {
+			str = words[i];
+			// Check whether each one can be spelled using level.charDict
+			if (WordLevel.CheckWordInLevel(str, level)) {
+				level.subWords.Add(str);
+			}
+			// Yield if we have parsed a lot of words this frame
+			if (i % WordList.NUM_TO_PARSE_BEFORE_YIELD == 0) {
+				// yield until the next frame
+				yield return null;
+			}
+		}
+		
+		level.subWords.Sort();
+		level.subWords = SortWordsByLength(level.subWords).ToList();
+		
+		// The coroutine is complete, so call SubWordSearchComplete()
+		SubWordSearchComplete();
+	}
+	
+	// Use LINQ to sort the array and return a copy
+	public static IEnumerable<string> SortWordsByLength(IEnumerable<string> ws) {
+		ws = ws.OrderBy(s => s.Length);
+		return ws;
+	}
+
+	public void SubWordSearchComplete() {
+		mode = GameMode.levelPrep;
 	}
 }
